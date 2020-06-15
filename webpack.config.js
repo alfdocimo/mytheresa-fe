10a32a08eb64514f52c215f99fa13df11b0bc342
Alfredo <alfredo.narvaez@travelport.com>
2020-06-15 21:43:42 +0200

    fix: webpack config for path

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = (env) => {
     },
     resolve: { extensions: ["*", ".js", ".jsx"] },
     output: {
-      path: path.resolve(__dirname, "dist/"),
+      path: path.resolve(__dirname, "dist"),
       publicPath: "/dist/",
       filename: "bundle.js",
     },
